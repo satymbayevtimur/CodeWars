6b722019a0bca1063295b8d800edbee3cdbc3913
satymbayevtimur <satymbayevtimur05@gmail.com>
2024-07-26 11:32:24 +0500

    Create String repeat.go

---
+++ Go/8-kyu/String repeat.go
@@ -0,0 +1,11 @@
+package kata
+
+func RepeatStr(repetitions int, value string) string {
+  var result string
+  
+  for i := 0; i < repetitions; i++ {
+    result += value
+  }
+  
+  return result
+}
